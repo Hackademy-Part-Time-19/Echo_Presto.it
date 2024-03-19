ca2577d457ea9a6260cfc972e4d332ae901d9578
Vincenzo Tito <vincenzotito@icloud.com>
2024-03-19 19:44:30 +0100

    si comincia

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-success-subtle" style="background-color:green !important; color:white !important ">
+
+    <div style="display:flex; align-items:center; justify-content:space-between; bac" class="container-fluid">
+        <div style="display:flex; align-items:center; ">
+            <a class="navbar-brand" href="{{ route('home') }}">Presto.it</a>
+            <a style="margin-left: 50px" class="nav-link active" aria-current="page" href="#">Annunci</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    </div>
+    <div style="margin-right: 50px" class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <button style="background-color: #2e9940; border-radius:15px;" > <a class="nav-link " href="#">Login</a></button>
+            <button style="background-color: #2e9940; border-radius:15px;" > <a class="nav-link " href="#">Registrati</a></button>
+        </div>
+    </div>
+
+    </div>
+</nav>
