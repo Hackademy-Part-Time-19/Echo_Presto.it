c23badda3f244ad72248916df1a0e32b03f9be17
Ivan <ivanlajoya10@gmail.com>
2024-03-21 15:30:51 +0100

    relazioni tra modelli

--- database/migrations/2024_03_20_200101_create_categories_table.php
+++ database/migrations/2024_03_20_200101_create_categories_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Category;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,6 +17,12 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        $categories = ['Elettronica', 'Arredamento', 'Libri', 'Musica', 'Sport', 'Giochi', 'Gioielli', 'Telefoni', 'Abbigliamento', 'Motori'];
+
+        foreach ($categories as $category) {
+            Category::create(['name'=>$category]);
+        }
     }
 
     /**
