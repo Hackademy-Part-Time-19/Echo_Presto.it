791d6aadf3b6a189785061aadcb173c526c318fb
Diego Lombardi <diegolombardi199@gmail.com>
2024-03-27 20:48:39 +0100

    footer modificato

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,30 +1,102 @@
-<footer style="background-color: #2a2a2a; height:350px; padding-top:30px;">
-    <div style="display:flex; align-items:center;justify-content:space-evenly; width:100%"">
-        <div>
-            <h2 style="color: white">Team di Sviluppo</h2>
-            <ul style="color: white; list-style:none">
-                <li>Angelicchio Maicol</li>
-                <li>Di Domenico Ivan</li>
-                <li>Lombardi Diego Franco</li>
-                <li>Tito Vincenzo</li>
-            </ul>
-        </div>
-        <div>
-            <h2 style="color: white">Contatti</h2>
-            <ul style="color: white; list-style:none">
-                <li>maicol.angelicchio.91@gmail.com</li>
-                <li>ivanlajoya10@gmail.com</li>
-                <li>diegolombardi199@gmail.com</li>
-                <li>vincenzotito@icloud.com</li>
-            </ul>
-        </div>
-    </div>
+<div>
+    <div class="pg-footer">
+        <footer class="footer">
+            <svg class="footer-wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 100"
+                preserveAspectRatio="none">
+                <path class="footer-wave-path"
+                    d="M851.8,100c125,0,288.3-45,348.2-64V0H0v44c3.7-1,7.3-1.9,11-2.9C80.7,22,151.7,10.8,223.5,6.3C276.7,2.9,330,4,383,9.8 c52.2,5.7,103.3,16.2,153.4,32.8C623.9,71.3,726.8,100,851.8,100z">
+                </path>
+            </svg>
+            <div class="footer-content">
+                <div class="footer-content-column">
+                    <div class="footer-logo">
+                        <a class="footer-logo-link"href="{{ route('home') }}">
+                            <img src="Images/LogoPresto4.png" style="width:100px;height:100px;">
+                        </a>
+                    </div>
 
-    <div style="width: 100%; display:flex; justify-content:center; align-items:center; margin-top:40px;">
-        <p style="align-self:end;color:white;  width:100%; display:flex; justify-content:center; align-items:center;"> Presto.it - Lavora con noi - Registrati e clicca qui  <a style="padding-left: 20px; color" href="{{route('work')}}">Diventa Revisore</a> 
-    </div>
+                    <div style="display: flex;flex-direction:row;width:1300px;justify-content:center;align-items:center">
 
-    <div style="width: 100%; display:flex; justify-content:center; margin-top:30px;">
-        <p style="align-self:end;color:white; padding-right:20px; width:14%; display:flex; justify-content:center; align-items:center; "> © 2024 Copyright: Presto.it</p>
+                        <div class="footer-menu" style="flex:3;align-items:center;">
+                            <h2 class="footer-menu-name">TEAM DI SVILUPPO</h2>
+                            <ul id="menu-get-started" class="footer-menu-list">
+                                <li>
+                                    <a class="styleAfooter"
+                                        href="https://www.linkedin.com/in/vincenzo-tito-junior-developer/">Vincenzo
+                                        Tito</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="#">Maicol Angelicchio</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="www.linkedin.com/in/ivan-di-domenicodeveloper">Ivan Di
+                                        Domenico</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="#">Diego Lombardi</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="footer-menu" style="flex:8;display:flex;flex-direction:column;align-items:center;">
+                            <h2 class="footer-menu-name">TEAM DI SVILUPPO</h2>
+                            <ul id="menu-get-started" class="footer-menu-list">
+                                <li>
+                                    <a class="styleAfooter"
+                                        href="https://www.linkedin.com/in/vincenzo-tito-junior-developer/">Vincenzo
+                                        Tito</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="#">Maicol Angelicchio</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="www.linkedin.com/in/ivan-di-domenicodeveloper">Ivan Di
+                                        Domenico</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="#">Diego Lombardi</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="footer-menu" style="flex:3;display:flex;flex-direction:column;align-items:center;">
+                            <h2 class="footer-menu-name">TEAM DI SVILUPPO</h2>
+                            <ul id="menu-get-started" class="footer-menu-list">
+                                <li>
+                                    <a class="styleAfooter"
+                                        href="https://www.linkedin.com/in/vincenzo-tito-junior-developer/">Vincenzo
+                                        Tito</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="#">Maicol Angelicchio</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="www.linkedin.com/in/ivan-di-domenicodeveloper">Ivan Di
+                                        Domenico</a>
+                                </li>
+                                <li>
+                                    <a class="styleAfooter" href="#">Diego Lombardi</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+
+        </footer>
+        <div class="footer-copyright">
+            <div class="footer-copyright-wrapper">
+                <p class="footer-copyright-text">
+                    <a class="footer-copyright-link" href="#" target="_self"> ©2020. | Designed By: Pooja Nahelia.
+                        | All rights reserved. </a>
+                </p>
+            </div>
+        </div>
     </div>
-</footer>
+</div>
