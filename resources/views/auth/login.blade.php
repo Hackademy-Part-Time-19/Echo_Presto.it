a5527455f9ba384039b6b504b3fb6f9473b67d01
Ivan <ivanlajoya10@gmail.com>
2024-03-28 20:57:57 +0100

    modifiche register e login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,21 @@
 <x-layout>
 
-    <div style=" background: linear-gradient(#030604,#1C6F29); height: 600px; " class="container">
+    <div style=" background-image: url('https://assets-global.website-files.com/5a9ee6416e90d20001b20038/64d14feca4cb71202bb97e89_%20-%204.jpg'); background-size:cover; background-repeat: no-repeat; height: 600px; width:100%;" class="container">
 
-        <div class="row justify-content-center mt-2">
+        <div class="row justify-content-center mt-2" style="margin-left: 90px;">
             <div style="flex:3; ">
-                <img style=" width:100%; height:100%;padding:40px;"
-                    src="https://www.samo-workshop.ch/images/profil-platzhalter-c.png"
+                <img style=" width:100%; height:100%; border-top-left-radius:20px;border-bottom-left-radius:20px; box-shadow: 4px 4px 8px rgba(0, 36, 12, 1.5);"
+                    src="https://knowledgemission.kerala.gov.in/img/official-login.jpg"
                     alt="">
             </div>
-            <div style="padding:30px; color:white" class="col-md-8">
+            <div style="padding:30px; color:white; background-color:rgba(0, 121, 40, 0.5);border-top-right-radius:20px;border-bottom-right-radius:20px" class="col-md-8">
                 <form action="{{ route('login') }}" method="POST">
                     @csrf
-                    <h2 class="m-3">Login</h2>
+                    <h2 >Accedi su <span style="font-weight: 600">Presto.it</span></h2>
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="email">Email</label>
-                            <input type="text" class="form-control" id="name" placeholder="Nome" name="email">
+                            <label for="email">La tua Email</label>
+                            <input type="text" class="form-control" id="name" placeholder="Email" name="email">
                         </div>
                         <div class="form-group col-md-6">
                             <label for="password">Password</label>
@@ -24,7 +24,7 @@
                         </div>
                     </div>
                     <div class="form-group col-md-6 mt-3">
-                        <button style="background-color: #389147; font-weight:bold" type="submit"
+                        <button style="background-color: rgb(37, 37, 223); font-weight:bold;border:solid 1px darkblue" type="submit"
                             class="btn btn-success">Accedi</button>
                     </div>
                 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,17 @@
 <x-layout>
 
-    <div style="display:flex; align-items:center;justify-content:center; background: linear-gradient(#030604,#1C6F29);"
-        class="container mt-5">
-        <div style="flex:5; ">
-            <img style=" width:100%; height:100%;padding:40px"
-                src="https://www.kinetica.it/wp-content/uploads/2018/02/img_tastiera-1170x731.jpg" alt="">
+    <div style="display:flex; align-items:center;justify-content:center; background-image: url('https://assets-global.website-files.com/5a9ee6416e90d20001b20038/64d14feca4cb71202bb97e89_%20-%204.jpg'); background-size:cover; background-repeat: no-repeat; height:500px" class="container mt-5">
+        <div style="flex:5; margin-left:40px; margin-right:10px">
+            <img style=" width:100%;  border-top-left-radius:20px;border-bottom-left-radius:20px; box-shadow: 4px 4px 8px rgba(0, 36, 12, 1.5);"
+                src="https://static.vecteezy.com/ti/vettori-gratis/p1/3689251-registrazione-online-o-registrazione-login-per-account-su-smartphone-app-interfaccia-utente-con-password-sicura-applicazione-mobile-per-interfaccia-utente-accesso-banner-web-cartoon-persone-illustrazione-vettoriale.jpg" alt="">
         </div>
-        <div class="row justify-content-center mt-3 " style="flex:6;padding:40px; width:100%">
+        <div  style="flex:6;display:flex;flex-direction:column;background-color:rgba(0, 121, 40, 0.5);border-top-right-radius:20px;border-bottom-right-radius:20px; padding:20px">
             <form style="color: white;" action="{{ route('register') }}" method="POST">
                 @csrf
-                <h2 class="m-3">Registrazione</h2>
+                <h2 style="font-weight: 600">Registrati</h2>
                 <div class="form-row ">
                     <div class="form-group col-md-9   ">
-                        <label for="name">Nome</label>
+                        <label for="name">Il tuo nome</label>
                         <input type="text" class="form-control " id="name" placeholder="Nome" name="name">
                     </div>
                 </div>
@@ -21,7 +20,7 @@
                 @enderror
                 <div class="form-row">
                     <div class="form-group col-md-9">
-                        <label for="inputEmail4">Email</label>
+                        <label for="inputEmail4">La tua Email</label>
                         <input type="email" class="form-control" id="email" placeholder="Email" name="email">
                     </div>
                     @error('email')
@@ -38,18 +37,17 @@
                     <div class="form-group col-md-9">
                         <label for="inputPassword4"> Conferma Password</label>
                         <input type="password" class="form-control" id="password_confirmation"
-                            placeholder=" conferma Password" name="password_confirmation">
+                            placeholder="Conferma Password" name="password_confirmation">
                     </div>
                     @error('password_confirmation')
                     <span class="small text-danger">{{ $message }}</span>
                 @enderror
                 </div>
-                <button style="margin-top:20px;background-color: #389147; font-weight:bold" type="submit"
-                    class="btn btn-success">Crea Account</button>
-            </form>
-
-
+                <button style="margin-top:20px;background-color: rgb(37, 37, 223); font-weight:bold;border:solid 1px darkblue" type="submit"
+                    class="btn btn-success">Registrati</button>
+            </form>          
         </div>
+
     </div>
 
 
