1d637144234f111a6e6b6858c371c4341415e62a
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-04-03 20:46:44 +0200

    aggiornato profilo, manca creazione su utente

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,12 +1,36 @@
 <x-layout>
     <ul class="list-group">
-        <li class="list-group-item"><strong>Name:</strong> {{ $profile->name }}</li>
-        <li class="list-group-item"><strong>Surname:</strong> {{ $profile->surname }}</li>
+        <li class="list-group-item"><strong>Nome:</strong>
+            @if(isset($profile) && $profile->name)
+            {{ $profile->name }}
+            @endif
+        </li>
+        <li class="list-group-item"><strong>Cognome:</strong>
+            @if(isset($profile) && $profile->surname)
+            {{ $profile->surname }}
+            @endif
+        </li>
         <li class="list-group-item"><strong>Email:</strong> {{ auth()->user()->email }}</li>
-        <li class="list-group-item"><strong>City:</strong> {{ $profile->city }}</li>
-        <li class="list-group-item"><strong>Address:</strong> {{ $profile->address }}</li>
-        <li class="list-group-item"><strong>Description:</strong> {{ $profile->description }}</li>
-        <li class="list-group-item"><strong>Advertisements:</strong> {{ $profile->advertisements_placed }}</li>
-        <li style="margin: 1%;"><a href="{{ route('user.update')}}" class="btn btn-info">Modifica</a></li>
+        <li class="list-group-item"><strong>Città:</strong>
+            @if(isset($profile) && $profile->city)
+                {{ $profile->city }}
+            @endif
+        </li>
+        <li class="list-group-item"><strong>Indirizzo:</strong>
+            @if(isset($profile) && $profile->address)
+                {{ $profile->address }}
+            @endif
+        </li>
+        <li class="list-group-item"><strong>Descrizione:</strong>
+             @if(isset($profile) && $profile->description)
+                {{ $profile->description }}
+            @endif
+        </li>
+        <li class="list-group-item"><strong>Annunci:</strong>
+            @if(isset($profile) && $profile->advertisements_placed )
+                {{ $profile->advertisements_placed  }}
+            @endif
+        </li>
+        <li style="margin: 1%;"><a href="{{ route('user.update',['id' => $profile->id] )}}" class="btn btn-info" style="margin-top:20px;background-color: rgb(37, 37, 223); font-weight:bold; border:solid 1px darkblue; color:white">Modifica</a></li>
     </ul>
 </x-layout>
