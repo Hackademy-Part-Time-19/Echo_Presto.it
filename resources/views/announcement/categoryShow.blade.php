b357ff0cbb1f01d4f94b5b758368e81ca5cc8d61
Vincenzo Tito <vincenzotito@icloud.com>
2024-04-03 20:51:10 +0200

    user 5 finita

--- resources/views/announcement/categoryShow.blade.php
+++ resources/views/announcement/categoryShow.blade.php
@@ -16,7 +16,7 @@
                         style="width: 24rem;  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset; ">
                             <div class="card-body">
                                 <img style="border-radius:5px; " class="img-fluid"
-                                    src="https://picsum.photos/500/400"
+                                    src={{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path) : "https://picsum.photos/400/400"}}
                                     alt="immagine">
                                 <h3 class="card-title text-center">{{ $announcement->title }}</h3>
                                 <h5 class="card-title text-center">{{ $announcement->body }}</h5>
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -14,44 +14,52 @@
                 <div style="display:flex; align-items:center; justify-content:center;"" class="col-12 d-flex">
                     <div class="card m-4 col-12 col-md-6 row m-2  d-flex  text-center" style="width: 50rem; ">
                         <div class="card-body">
-                            <div id="showCarousel" class="carousel slide" data-bs-slide="carousel">
+                            <div style="padding: 12px" id="carouselExampleIndicators" class="carousel slide">
+                                <div class="carousel-indicators">
+                                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                </div>
                                 @if ($announcement_to_check->images)
                                 <div class="carousel-inner">
-                                    @foreach($announcement_to_check->images as $image)
-                                    <div class="carousel-item @if($loop->first) active @endif">
-                                    <img src="{{Storage::url($image->path)}}" class="img-fluid p-3 rounded" alt="">
-                                    </div>
+                                    @foreach ($announcement_to_check->images as $image)
+                                        <div class="carousel-item @if ($loop->first) active @endif">
+                                            <img src="{{ Storage::url($image->path) }}"
+                                                class="img-fluid p-3 rounded" alt="">
+                                        </div>
                                     @endforeach
                                 </div>
-                                @else
+                            @else
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img src="https://picsum.photos/500/400" class="img-fluid p-3 rounded" alt="">
+                                        <img src="https://picsum.photos/500/400" class="img-fluid p-3 rounded"
+                                            alt="">
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="https://picsum.photos/500/400" class="img-fluid p-3 rounded" alt="">
+                                        <img src="https://picsum.photos/500/400" class="img-fluid p-3 rounded"
+                                            alt="">
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="https://picsum.photos/500/400" class="img-fluid p-3 rounded" alt="">
+                                        <img src="https://picsum.photos/500/400" class="img-fluid p-3 rounded"
+                                            alt="">
                                     </div>
                                 </div>
-                                @endif
-                                <button class="carousel-control-prev" type="button" data-bs-target="#shoeCarousel"
-                                    data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
+                            @endif
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#shoeCarousel"
-                                    data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Next</span>
                                 </button>
-                            </div>
-                            
+                              </div>
+
                             <h3 class="card-title text-center">{{ $announcement_to_check->title }}</h3>
                             <h5 class="card-title text-center">{{ $announcement_to_check->body }}</h5>
                             <p class="card-text">{{ $announcement_to_check->price }} €</p>
-                            <p class="card-text"><a style="text-decoration: none; color:black;" href="{{route('categoryShow',['category'=>$announcement_to_check->category])}}">
+                            <p class="card-text"><a style="text-decoration: none; color:black;"
+                                    href="{{ route('categoryShow', ['category' => $announcement_to_check->category]) }}">
                                     Categoria : {{ $announcement_to_check->category->name }}</a></p>
                             <p style="color: white; background-color:#6CA7DD" class="card-footer">Pubblicato il:
                                 {{ $announcement_to_check->created_at->format('d/m/Y') }} <br> Autore:
@@ -81,7 +89,8 @@
                                             method="POST">
                                             @csrf
                                             @method('PATCH')
-                                            <button style="margin-left: 50px" type="submit" class="btn btn-danger shadow">Rifiuta</button>
+                                            <button style="margin-left: 50px" type="submit"
+                                                class="btn btn-danger shadow">Rifiuta</button>
                                         </form>
                                     </div>
                                 </div>
