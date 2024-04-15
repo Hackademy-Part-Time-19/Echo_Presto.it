0c87b725ba69d877f06b2d02c061fad110f2ec27
Ivan <ivanlajoya10@gmail.com>
2024-04-15 20:35:26 +0200

    remove dd

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -22,7 +22,7 @@ class AnnouncementController extends Controller
     public function showAnnouncement (Announcement $announcement)
     {
         $announcements = Announcement::where('purchased', 0)->get();
-        dd($announcement);
+        
         return view('announcement.dettaglio', compact('announcement'));
     }
 
